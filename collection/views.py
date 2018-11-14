6e9a90ffbb88dac37d79305d25552c509615a089
Sam LoCascio <sflocascio@gmail.com>
2018-11-13 21:50:41 -0500

    submitting 1st day of hello web app

---
+++ collection/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def index(request):
+    number = 6
+    thing = "Thing name"
+    return render(request, 'index.html', {
+        'number': number,
+        'thing': thing,
+    })
